6a512f6b3976811322a92c615c55819f9574cb5e
imran-basha66 <ibasha66@gmail.com>
2020-01-29 18:03:53 +0530

    Added 404 page

--- src/router/index.js
+++ src/router/index.js
@@ -2,15 +2,24 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Meta from 'vue-meta'
 import Home from './../components/Home'
+import PageNotFound from './../components/pages/404'
 
 Vue.use(Router)
 Vue.use(Meta)
 
 const router = new Router({
-  routes: [{
-    path: '/',
-    component: Home
-  }]
+  base: '/',
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      component: Home
+    },
+    {
+      path: '*',
+      component: PageNotFound
+    }
+  ]
 })
 
 export default router
