fa92c698e424e8c409367754122f502139bd2a5d
Imran Basha <ibasha66@gmail.com>
2020-02-13 17:48:46 +0530

    Adding loader for insta images (#29)

--- src/components/layouts/Gallery.vue
+++ src/components/layouts/Gallery.vue
@@ -9,7 +9,17 @@
     <carousel-3d v-if="showImages" :width="350" :height="300" :display="7" :autoplay="true" :autoplay-hover-pause="false" :border="0">
       <slide v-for="(image, i) in imageList" :key="i" :index="i">
         <a :href="image.link" target="_blank">
-          <v-img :src="image.images.standard_resolution.url" :lazy-src="image.images.thumbnail.url" class="insta-img" />
+          <v-img :src="image.images.standard_resolution.url" :lazy-src="image.images.thumbnail.url" class="insta-img">
+            <template v-slot:placeholder>
+              <b-row align-v="center" align-h="center" class="h-100">
+                <fulfilling-bouncing-circle-spinner
+                  :animation-duration="4000"
+                  :size="40"
+                  color="#dc3545"
+                />
+              </b-row>
+            </template>
+          </v-img>
         </a>
         <figcaption>
           <b-row class="text-center">
@@ -34,8 +44,12 @@
 </template>
 
 <script>
+import { FulfillingBouncingCircleSpinner } from 'epic-spinners'
 export default {
   name: 'Gallery',
+  components: {
+    FulfillingBouncingCircleSpinner
+  },
   data: () => ({
     imageList: [],
     showImages: false
