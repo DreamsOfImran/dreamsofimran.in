75b8d8f01d5c53552f664ed245d62d666fad8132
Imran Basha <ibasha66@gmail.com>
2020-04-07 10:21:29 +0530

    Fix the icons missing issue (#32)

--- src/components/layouts/Footer.vue
+++ src/components/layouts/Footer.vue
@@ -5,7 +5,7 @@
         <ul class="list-unstyled">
           <li v-for="social in socialLinks" :key="social.id">
             <a :href="social.url" target="_blank">
-              <v-icon medium>mdi-{{ social.name }}</v-icon>
+              <v-icon medium>fab fa-{{ social.name }}</v-icon>
             </a>
           </li>
         </ul>
@@ -26,12 +26,12 @@ export default {
   name: 'Footer',
   data: () => ({
     socialLinks: [
-      { id: 1, name: 'facebook-box', url: 'https://www.facebook.com/DreamsOfImran' },
+      { id: 1, name: 'facebook-square', url: 'https://www.facebook.com/DreamsOfImran' },
       { id: 2, name: 'twitter', url: 'https://twitter.com/DreamsOfImran' },
       { id: 3, name: 'instagram', url: 'https://www.instagram.com/dreamsofimran' },
-      { id: 4, name: 'github-box', url: 'https://github.com/DreamsOfImran' },
-      { id: 5, name: 'medium', url: 'https://medium.com/@DreamsOfImran' },
-      { id: 6, name: 'dev-to', url: 'https://dev.to/dreamsofimran' }
+      { id: 4, name: 'github-square', url: 'https://github.com/DreamsOfImran' },
+      { id: 5, name: 'medium-m', url: 'https://medium.com/@DreamsOfImran' },
+      { id: 6, name: 'dev', url: 'https://dev.to/dreamsofimran' }
     ]
   })
 }
