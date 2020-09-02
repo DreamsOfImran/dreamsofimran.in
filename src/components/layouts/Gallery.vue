884fe109b41a65ee796fef151048ae09474c5ad3
DreamsOfImran <ibasha66@gmail.com>
2020-09-02 09:17:24 +0530

    Fixing images from instagram

--- src/components/layouts/Gallery.vue
+++ src/components/layouts/Gallery.vue
@@ -8,8 +8,8 @@
 
     <carousel-3d v-if="showImages" :width="350" :height="300" :display="7" :autoplay="true" :autoplay-hover-pause="false" :border="0">
       <slide v-for="(image, i) in imageList" :key="i" :index="i">
-        <a :href="image.link" target="_blank">
-          <v-img :src="image.images.standard_resolution.url" :lazy-src="image.images.thumbnail.url" class="insta-img">
+        <a :href="image.permalink" target="_blank">
+          <v-img :src="image.media_url" :lazy-src="image.media_url" class="insta-img">
             <template v-slot:placeholder>
               <b-row align-v="center" align-h="center" class="h-100">
                 <fulfilling-bouncing-circle-spinner
@@ -21,23 +21,19 @@
             </template>
           </v-img>
         </a>
-        <figcaption>
+        <!-- <figcaption>
           <b-row class="text-center">
             <b-col>
-              <v-icon medium class="text-danger">
-                mdi-heart
-              </v-icon>
+              <v-icon medium class="text-danger">mdi-heart</v-icon>
               {{ image.likes.count }}
             </b-col>
 
             <b-col>
-              <v-icon medium class="text-danger">
-                mdi-comment-outline
-              </v-icon>
+              <v-icon medium class="text-danger">mdi-comment-outline</v-icon>
               {{ image.comments.count }}
             </b-col>
           </b-row>
-        </figcaption>
+        </figcaption>-->
       </slide>
     </carousel-3d>
   </div>
@@ -59,9 +55,14 @@ export default {
   },
   methods: {
     fetchInstaImages() {
+      let media_fields = ["id", "media_type", "media_url", "permalink"]
+      // this.$http
+      //   .get(
+      //     `https://api.instagram.com/v1/users/self/media/recent?access_token=${process.env.VUE_APP_INSTAGRAM_ACCESS_TOKEN}&count=50`
+      //   )
       this.$http
         .get(
-          `https://api.instagram.com/v1/users/self/media/recent?access_token=${process.env.VUE_APP_INSTAGRAM_ACCESS_TOKEN}&count=50`
+          `https://graph.instagram.com/me/media?fields=${media_fields.join()}&access_token=${process.env.VUE_APP_INSTAGRAM_ACCESS_TOKEN}`
         )
         .then(response => {
           this.imageList = response.data.data
