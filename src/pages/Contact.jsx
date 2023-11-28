424606e03cfdec4cc35d0033f1026af3f711c37a
Imran Basha <ibasha66@gmail.com>
2023-11-29 00:12:12 +0530

    Changing to_name for emailjs template

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "DreamsOfImran Portfolio",
+          to_name: "Imran",
           from_email: form.email,
           to_email: "ibasha66@gmail.com",
           message: form.message,
